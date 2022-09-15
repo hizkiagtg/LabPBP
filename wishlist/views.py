7a490f98b9909a21b9df3312d94b4efcccbb4297
Hizkia Sebastian Ginting <hizkia.sebastian@ui.ac.id>
2022-09-15 15:05:46 +0700

    Update urls and views in wishlist

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,12 +1,29 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render
-from wishlist.models import BarangWishlist
+from wishlist.models import BarangWishlist 
 
 # Create your views here.
 def show_wishlist(request):
+    data = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data,
+        'nama': 'Hizkia Sebastian Ginting'
+    }
     return render(request, "wishlist.html", context)
 
-data_barang_wishlist = BarangWishlist.objects.all()
-context = {
-    'list_barang': data_barang_wishlist,
-    'nama': 'Hizkia Sebastian Ginting'
-}
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
